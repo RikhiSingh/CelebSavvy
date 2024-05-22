be5f7ebc85ead46db70cb8907766a2825f02e482
rikhisingh <rikhisinghnov@gmail.com>
2024-05-22 10:54:43 -0400

    mobile sidebar completed

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -1,7 +1,21 @@
+import { Menu } from "lucide-react";
+
+import{
+    Sheet,
+    SheetContent,
+    SheetTrigger
+} from "@/components/ui/sheet";
+import { Sidebar } from "@/components/sidebar";
+
 export const MobileSidebar = () =>{
     return(
-        <div>
-            Mobile
-        </div>
+        <Sheet>
+            <SheetTrigger className="md:hidden pr-4">
+                <Menu />
+            </SheetTrigger>
+            <SheetContent side='left' className="p-0 bg-secondary pt-10 w-32">
+                <Sidebar />
+            </SheetContent>
+        </Sheet>
     );
 };
