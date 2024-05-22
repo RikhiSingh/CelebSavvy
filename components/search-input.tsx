1247f061b1e95da5a62dca46306f317171b315d2
rikhisingh <rikhisinghnov@gmail.com>
2024-05-22 13:42:58 -0400

    created search-input

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -1,9 +1,16 @@
 "use client";
 
-export const SearchInput = () =>{
-    return(
-        <div>
-            Search Input!
+import { SearchIcon } from "lucide-react";
+import { Input } from "@/components/ui/input";
+
+export const SearchInput = () => {
+    return (
+        <div className="relative">
+            <SearchIcon className="absolute h-4 w-4 top-3 left-4 text-muted-foreground" />
+            <Input 
+                placeholder="Search..."
+                className="pl-10 bg-primary/10"
+            />
         </div>
     );
 };
