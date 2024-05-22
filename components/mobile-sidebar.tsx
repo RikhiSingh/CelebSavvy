e992fb956d756cf84a43ab6e0470a898e2d69e10
rikhisingh <rikhisinghnov@gmail.com>
2024-05-22 10:52:00 -0400

    mobile sidebar skeleton created

---
+++ components/mobile-sidebar.tsx
@@ -0,0 +1,7 @@
+export const MobileSidebar = () =>{
+    return(
+        <div>
+            Mobile
+        </div>
+    );
+};
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -8,6 +8,7 @@ import { UserButton } from "@clerk/nextjs";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "@/components/mode-toggle";
+import { MobileSidebar } from "./mobile-sidebar";
 
 const font = Poppins({
     weight: "600",
@@ -18,7 +19,7 @@ export const Navbar = () => {
     return (
         <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary-10 bg-secondary h-16">
             <div className="flex items-center">
-                <Menu className="block md:hidden" />
+                <MobileSidebar />
                 <Link href="/">
                     <h1 className={cn(
                         "hidden md:block text-xl md:text-3xl font-bold text-primary",
