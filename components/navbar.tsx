f8b093535aaf21b07ca9886ee9db174b3e447f0c
rikhisingh <rikhisinghnov@gmail.com>
2024-05-22 10:13:29 -0400

    navbar logo and userbutton completed

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,34 @@
 "use client";
 
-export const Navbar = () =>{
-    return(
-        <div>
-            Navbar
+import { Menu } from "lucide-react";
+import { Poppins } from "next/font/google";
+import Link from "next/link";
+import { UserButton } from "@clerk/nextjs";
+
+import { cn } from "@/lib/utils";
+
+const font = Poppins({
+    weight: "600",
+    subsets: ["latin"]
+});
+
+export const Navbar = () => {
+    return (
+        <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary-10 bg-secondary">
+            <div className="flex items-center">
+                <Menu className="block md:hidden" />
+                <Link href="/">
+                    <h1 className={cn(
+                        "hidden md:block text-xl md:text-3xl font-bold text-primary",
+                        font.className
+                    )}>
+                        CelebSavvy
+                    </h1>
+                </Link>
+            </div>
+            <div className="flex items-center gap-x-3">
+                    <UserButton />
+            </div>
         </div>
     );
 };
