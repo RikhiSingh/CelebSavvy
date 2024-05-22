1f5ca6fc1363700694a8f8ad1006b40ee3105d99
rikhisingh <rikhisinghnov@gmail.com>
2024-05-22 10:29:35 -0400

    theme selector working

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -7,6 +7,7 @@ import { UserButton } from "@clerk/nextjs";
 
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/mode-toggle";
 
 const font = Poppins({
     weight: "600",
@@ -32,6 +33,7 @@ export const Navbar = () => {
                     Upgrade 
                     <Sparkles className="h-4 w-4 fill-white text-white ml-2"/>
                 </Button>
+                <ModeToggle />
                     <UserButton />
             </div>
         </div>
