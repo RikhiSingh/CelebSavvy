066f719c7b1ada0298c513ca78a5ed37bafb58e0
rikhisingh <rikhisinghnov@gmail.com>
2024-05-22 13:45:54 -0400

    constants created for search

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -1,13 +1,24 @@
 "use client";
 
 import { SearchIcon } from "lucide-react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
+
 import { Input } from "@/components/ui/input";
 
 export const SearchInput = () => {
+    const router = useRouter();
+    const searchParams = useSearchParams();
+
+    const categoryId = searchParams.get("categoryId");
+    const name = searchParams.get("name");
+
+    const [value, setValue] = useState(name || "");
+
     return (
         <div className="relative">
             <SearchIcon className="absolute h-4 w-4 top-3 left-4 text-muted-foreground" />
-            <Input 
+            <Input
                 placeholder="Search..."
                 className="pl-10 bg-primary/10"
             />
