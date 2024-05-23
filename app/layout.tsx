a81ae2337a913225ca645476f75c98bdb8c1e8da
rikhisingh <rikhisinghnov@gmail.com>
2024-05-23 18:30:55 -0400

    added toaster to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 
 import { ThemeProvider } from "@/components/theme-provider";
 import { cn } from "@/lib/utils";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,9 +22,10 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
-        <body className={cn("bg-secondary",inter.className)}>
+        <body className={cn("bg-secondary", inter.className)}>
           <ThemeProvider attribute="class" forcedTheme="system" enableSystem>
             {children}
+            <Toaster />
           </ThemeProvider>
         </body>
       </html>
