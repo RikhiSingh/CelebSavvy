7983a3a5fddd9b4657256e41f35b3db3259f2d8c
rikhisingh <rikhisinghnov@gmail.com>
2024-05-23 14:02:45 -0400

    category finished

---
+++ scripts/seed.ts
@@ -0,0 +1,27 @@
+const { PrismaClient } = require("@prisma/client");
+
+const db = new PrismaClient();
+
+async function main(){
+    try{
+        await db.category.createMany({
+            data:[
+                {name:"Famous People"},
+                {name:"Movies & TV"},
+                {name:"Musicians"},
+                {name:"Games"},
+                {name:"Sports"},
+                {name:"Animals"},
+                {name:"Scientists"},
+                {name:"Tech"},
+                {name:"Philosophy"},
+            ]
+        })
+    }catch(error){
+        console.error("Error seeding default categories", error);
+    }finally{
+        await db.$disconnect();
+    }
+};
+
+main();
