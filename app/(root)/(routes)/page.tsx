329e36121ccaa2636b0ef980c1740b37b2c7423b
rikhisingh <rikhisinghnov@gmail.com>
2024-05-24 14:35:41 -0400

    added search params

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -2,10 +2,17 @@ import { Categories } from "@/components/categories";
 import { SearchInput } from "@/components/search-input";
 import prismadb from "@/lib/prismadb";
 
-const RootPage = async () => {
-    const categories = await prismadb.category.findMany({
+interface RootPageProps{
+    searchParams:{
+        categoryId: string,
+        name: string;
+    };
+};
 
-    });
+const RootPage = async ({
+    
+}) => {
+    const categories = await prismadb.category.findMany();
 
     return (
         <div className="h-full p-4 space-y-2">
