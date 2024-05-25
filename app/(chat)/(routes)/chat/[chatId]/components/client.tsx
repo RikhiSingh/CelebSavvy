9bcc4307594dddb7101ded13e0d583315786bdad
rikhisingh <rikhisinghnov@gmail.com>
2024-05-25 13:23:43 -0400

    created props for chat client

--- app/(chat)/(routes)/chat/[chatId]/components/client.tsx
+++ app/(chat)/(routes)/chat/[chatId]/components/client.tsx
@@ -1,5 +1,16 @@
 "use client";
 
+import { Companion, Message } from "@prisma/client";
+
+interface ChatClientProps{
+    companion: Companion &{
+        messages: Message[],
+        _count:{
+            messages:number;
+        };
+    };
+};
+
 export const ChatClient = () =>{
     return(
         <div>
