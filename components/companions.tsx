cc3d0ae3d4b2b63f648ad0eb158bbb7461fcf7dc
rikhisingh <rikhisinghnov@gmail.com>
2024-05-25 12:16:32 -0400

    companion card finished

--- components/companions.tsx
+++ components/companions.tsx
@@ -1,5 +1,8 @@
 import { Companion } from "@prisma/client";
 import Image from "next/image";
+import { Card, CardFooter, CardHeader } from "@/components/ui/card";
+import Link from "next/link";
+import { MessagesSquare } from "lucide-react";
 
 interface CompanionProps {
     data: (Companion & {
@@ -31,8 +34,41 @@ export const Companions = ({
     }
 
     return (
-        <div>
-            Companions
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-2 pb-10">
+            {data.map((item)=>(
+                <Card
+                    key={item.id}
+                    className="bg-primary/10 rounded-xl cursor-pointer hover:opacity-75 transition border-0"
+                >
+                    <Link href={`/chat/${item.id}`}>
+                        <CardHeader className="flex items-center justify-center text-center text-muted-foreground">
+                            <div className="relative w-32 h-32">
+                                <Image 
+                                    src={item.src}
+                                    fill
+                                    className="rounded-xl object-cover"
+                                    alt="Companion"
+                                />
+                            </div>
+                            <p className="font-bold">
+                                {item.name}
+                            </p>
+                            <p className="text-sm">
+                                {item.description}
+                            </p>
+                        </CardHeader>
+                        <CardFooter className="flex items-center justify-between text-xs text-muted-foreground">
+                            <p className="lowercase">
+                                @{item.userName}
+                            </p>
+                            <div className="flex items-center">
+                                <MessagesSquare className="w-3 h03 mr-1"/>
+                                {item._count.messages}
+                            </div>
+                        </CardFooter>
+                    </Link>
+                </Card>
+            ))}
         </div>
     );
 };
