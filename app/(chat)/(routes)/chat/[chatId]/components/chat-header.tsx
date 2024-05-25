17c1f528bbec452b1515a093289eb17785c2270c
rikhisingh <rikhisinghnov@gmail.com>
2024-05-25 13:28:59 -0400

    back button added to chat header

--- app/(chat)/(routes)/chat/[chatId]/components/chat-header.tsx
+++ app/(chat)/(routes)/chat/[chatId]/components/chat-header.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { Companion, Message } from "@prisma/client";
+import { ChevronLeft } from "lucide-react";
+import { useRouter } from "next/navigation";
 
-interface ChatHeaderProps{
-    companion: Companion &{
+interface ChatHeaderProps {
+    companion: Companion & {
         messages: Message[];
-        _count:{
+        _count: {
             messages: number;
         };
     };
@@ -13,10 +16,16 @@ interface ChatHeaderProps{
 
 export const ChatHeader = ({
     companion
-}: ChatHeaderProps) =>{
-    return(
-        <div>
-            Chat Header
+}: ChatHeaderProps) => {
+    const router = useRouter();
+
+    return (
+        <div className="flex w-full justify-between items-center border-b border-primary/10 pb-4">
+            <div className="flex gap-x-2 items-center">
+                <Button onClick={() => router.back()} size="icon" variant="ghost">
+                    <ChevronLeft className="h-8 w-8" />
+                </Button>
+            </div>
         </div>
     )
 }
--- app/(chat)/(routes)/chat/[chatId]/components/client.tsx
+++ app/(chat)/(routes)/chat/[chatId]/components/client.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Companion, Message } from "@prisma/client";
+
 import { ChatHeader } from "./chat-header";
 
 interface ChatClientProps{
