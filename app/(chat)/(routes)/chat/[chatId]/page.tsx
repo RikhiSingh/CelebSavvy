3f267b3ed5948632820b7e3ffc54eea6051c2a4e
rikhisingh <rikhisinghnov@gmail.com>
2024-05-25 13:20:58 -0400

    query created for companion

--- app/(chat)/(routes)/chat/[chatId]/page.tsx
+++ app/(chat)/(routes)/chat/[chatId]/page.tsx
@@ -1,9 +1,54 @@
-const ChatIdPage = () => {
-    return ( 
+import prismadb from "@/lib/prismadb";
+import { auth, redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+interface ChatIdPageProps {
+    params: {
+        chatId: string;
+    };
+};
+
+const ChatIdPage = async ({
+    params
+}: ChatIdPageProps) => {
+    const { userId } = auth();
+
+    if(!userId){
+        return redirectToSignIn();
+    };
+
+    const companion = await prismadb.companion.findUnique({
+        where:{
+            id:params.chatId
+        },
+        include:{
+            messages:{
+                orderBy:{
+                    createdAt: "asc",
+                },
+                // only load message messages between current user and this chat companion
+                where:{
+                    userId,
+                }
+            },
+            // messages count from all users
+            _count:{
+                select:{
+                    messages: true
+                }
+            }
+        }
+    });
+
+    if(!companion){
+        return redirect("/");
+    };
+
+    return (
         <div>
             Chat ID Page
         </div>
-     );
+    );
 }
- 
+
 export default ChatIdPage;
