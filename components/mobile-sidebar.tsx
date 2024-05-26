e0bb47e5a30592e23fc18a984823cf563f26493e
rikhisingh <rikhisinghnov@gmail.com>
2024-05-26 14:07:23 -0400

    fixed build issue

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -7,14 +7,18 @@ import{
 } from "@/components/ui/sheet";
 import { Sidebar } from "@/components/sidebar";
 
-export const MobileSidebar = () =>{
+export const MobileSidebar = ({
+    isPro
+}:{
+    isPro: boolean;
+}) =>{
     return(
         <Sheet>
             <SheetTrigger className="md:hidden pr-4">
                 <Menu />
             </SheetTrigger>
             <SheetContent side='left' className="p-0 bg-secondary pt-10 w-32">
-                <Sidebar />
+                <Sidebar isPro={isPro} />
             </SheetContent>
         </Sheet>
     );
