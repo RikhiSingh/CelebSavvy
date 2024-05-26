b0c95b17688b7d4e4012fdf7df2a33cc53810edb
rikhisingh <rikhisinghnov@gmail.com>
2024-05-26 13:17:39 -0400

    Stripe API finished

---
+++ app/api/stripe/route.ts
@@ -0,0 +1,70 @@
+import { auth, currentUser } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+import prismadb from "@/lib/prismadb";
+import { stripe } from "@/lib/stripe";
+import { absoluteUrl } from "@/lib/utils";
+
+const settingsUrl = absoluteUrl("/settings");
+
+export async function GET() {
+    try {
+        const { userId } = auth();
+        const user = await currentUser();
+
+        if (!userId || !user) {
+            return new NextResponse("Unauthorized", { status: 401 });
+        };
+
+        const userSubscription = await prismadb.userSubscription.findUnique({
+            where: {
+                userId
+            }
+        });
+
+        if (userSubscription && userSubscription.stripeCustomerId) {
+            const stripeSession = await stripe.billingPortal.sessions.create({
+                customer: userSubscription.stripeCustomerId,
+                return_url: settingsUrl,
+            });
+
+            return new NextResponse(JSON.stringify({ url: stripeSession.url }));
+        };
+
+        // first time
+        const stripeSession = await stripe.checkout.sessions.create({
+            success_url: settingsUrl,
+            cancel_url: settingsUrl,
+            payment_method_types: ["card"],
+            mode: "subscription",
+            billing_address_collection: "auto",
+            customer_email: user.emailAddresses[0].emailAddress,
+            line_items: [
+                {
+                    price_data: {
+                        currency: "CAD",
+                        product_data: {
+                            name: "CelebSavvy Pro",
+                            description: "Create Custom AI Celebs",
+                        },
+                        unit_amount: 690,
+                        recurring: {
+                            interval: "month",
+                        }
+                    },
+                    quantity: 1,
+                }
+            ],
+            // the return fire reading
+            metadata: {
+                userId
+            }
+        });
+
+        return new NextResponse(JSON.stringify({ url: stripeSession.url }));
+
+    } catch (error) {
+        console.log("[STRIPE_GET]", error);
+        return new NextResponse("Internal Error", { status: 500 });
+    }
+}
--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs/server";
 
-export default authMiddleware();
+export default authMiddleware({
+  // stripe aint a logged in user let it  access the webhook
+  publicRoutes: ['/api/webhook'],
+});
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
