bd97a0584b6474e25fdc557703b4f99c14d753d3
rikhisingh <rikhisinghnov@gmail.com>
2024-05-26 14:02:18 -0400

    responsive settings page

--- app/(root)/(routes)/settings/page.tsx
+++ app/(root)/(routes)/settings/page.tsx
@@ -5,7 +5,7 @@ const SettingsPage = async () => {
     const isPro = await checkSubscription();
 
     return (
-        <div className="h-full p-4 space-y-2 flex flex-col items-center gap-4">
+        <div className="sm:flex sm:flex-col sm:items-center sm:justify-center md:items-center md:justify-start h-full p-4 space-y-2 gap-4 ">
             <h3 className="text-2xl font-medium text-center">
                 Settings
             </h3>
