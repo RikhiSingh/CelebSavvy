af4f118d0b2d89733066c313f750416b6fd6a661
rikhisingh <rikhisinghnov@gmail.com>
2024-05-26 21:39:03 -0400

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Analytics } from '@vercel/analytics/react';
 
 import { ThemeProvider } from "@/components/theme-provider";
 import { cn } from "@/lib/utils";
@@ -29,6 +30,7 @@ export default function RootLayout({
             <PopUp />
             <ProModal />
             {children}
+            <Analytics />
             <Toaster />
           </ThemeProvider>
         </body>
